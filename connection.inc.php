121171c6c848a5151bbd8d32b00033026189f40d
raja <raja@raja.com>
2021-10-20 13:47:41 +0530

    done

--- connection.inc.php
+++ connection.inc.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
+if($_SERVER['HTTP_HOST']=='localhost'){
 $connection=mysqli_connect("localhost","root","","shreeji_ind_db");
+}
+else{
+    $connection=mysqli_connect("localhost","shreejiindustries_db","Raja83013@#","shreejiindustries_db");
+}
 if($connection){
     // echo"Connection Establish successfully";
 }
